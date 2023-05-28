7a5dc10c7a9b6782784dbd0c27114a8fd3e9aa8d
vsevolod22 <240303vilkov@gmail.com>
2023-05-28 19:29:42 +0300

    first project 1 step obj move

--- script.js
+++ script.js
@@ -1,69 +1,16 @@
 "use strict";
-// let number = 5;
-// const person = 'Alexander';
-// console.log(person);
-// const bool = true;
-// console.log(-bool);
-// const object = {
-//     name: 'vlad',
-//     surname: 'vill',
-//     age: 10,
-//     isMarried: false
-// };
-// console.log(object.age);
-// let arr = [1,3,54,1,2,3,54,33,2,231,11]
-// console.log(arr);
-// const arr = [1,2,3,3,1];
-// const arrobject = {
-//     0: 1,
-//     1: 2,
-//     3: 3,
-//     4: 4,
-//     5: 5
-// };
-// console.log(arr[1]);
-// const b = 'b';
-// console.log(arrobject[3]);
-// arrobject[b] = '123';
-// console.log(arrobject.b);
-
-
-// const object = {
-//     name: 'vlad',
-//     surname: 'vill',
-//     age: 10,
-//     isMarried: false
-// };
-// console.log(object.name);
-// alert('hello world');
-// 
-// const answer = +prompt('Вам есть 18?', '18');
-// console.log(answer + 10);
-
-// const answers = [];
-
-// answers[0] = prompt('Какое у вас имя?', '');
-// answers[1] = prompt('Какая у вас фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
-
-// document.write(answers);
-
-// const category = 'toys';
-// console.log(`https://someurl.com/${category}/5`);
-
-
-// console.log(4 + +'5');
-
-
-let incr = 10,
-    decr = 10;
-// ++incr;
-// --decr;
-console.log(++incr , --decr);
-console.log(5%2);
-console.log('5' === 5);
-
-
+const number = prompt("Сколько фильмов вы уже посмотрели?", '');
+const PersonalMovieDB = {
+    count: number,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const film = prompt("Один из последних просмотренных фильмов?", "");
+const mark = prompt("На сколько оцените его?", "");
+PersonalMovieDB.movies[film] = mark;
+console.log(PersonalMovieDB.movies)
 
 
 
