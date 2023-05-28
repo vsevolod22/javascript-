87969bbaf3f308fe344b757b605ad634d235d355
vsevolod22 <240303vilkov@gmail.com>
2023-05-28 22:53:47 +0300

    first project 2 step if and for

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 "use strict";
-const number = prompt("Сколько фильмов вы уже посмотрели?", '');
+let number = prompt("Сколько фильмов вы уже посмотрели?", '');
+while (true) {
+    if (number === '' || number === null || number === undefined || isNaN(+number)) {
+        number = prompt("Сколько фильмов вы уже посмотрели?", "");       
+    } else if (+number < 10) {
+        document.write("Просмотрено довольно мало фильмов");
+        break;
+    } else if (+number > 10 && number < 30) {
+        document.write("Вы классический зритель");
+        break;
+    } else if (+number > 30) {
+        document.write("Вы киноман");
+        break;
+    }
+
+}
 const PersonalMovieDB = {
     count: number,
     movies: {},
@@ -7,12 +22,23 @@ const PersonalMovieDB = {
     genres: [],
     privat: false
 };
-const film = prompt("Один из последних просмотренных фильмов?", "");
-const mark = prompt("На сколько оцените его?", "");
-PersonalMovieDB.movies[film] = mark;
-console.log(PersonalMovieDB.movies)
 
-
-
-
+for (let i = 0; i < number; i++) {
+    let film = prompt("Один из последних просмотренных фильмов?", "");
+    let mark = prompt("На сколько оцените его?", "");
+    console.log(+mark);
+    while (true) {
+        if (film === '' || film === null || film === undefined || film.length > 50) {  
+            film = prompt("Один из последних просмотренных фильмов?", "");      
+        }
+        else if (mark === '' || mark === null || mark === undefined || +mark > 10 || isNaN(+mark)) {
+            mark = prompt("На сколько оцените его?", "");       
+        }
+        else {
+            PersonalMovieDB.movies[film] = mark;
+            break;
+        }
+    }
+}
+console.log(PersonalMovieDB.movies);
 
